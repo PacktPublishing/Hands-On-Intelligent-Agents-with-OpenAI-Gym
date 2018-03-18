01c1a598a26d17d468a7ab9645f7d4ac922b8a79
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-03-18 14:07:12 -0400

    ch4. Added the bare minimum boilerplate code for Reinforcement Learning using OpenAI Gym

---
+++ ch4/rl_gym_boilerplate_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import gym
+env = gym.make("Qbert-v0")
+MAX_NUM_EPISODES = 10
+MAX_STEPS_PER_EPISODE = 500
+for episode in range(MAX_NUM_EPISODES):
+    obs = env.reset()
+    for step in range(MAX_STEPS_PER_EPISODE):
+        env.render()
+        action = env.action_space.sample()# Sample random action. This will be replaced by our agent's action when we start developing the agent algorithms
+        next_state, reward, done, info = env.step(action) # Send the action to the environment and receive the next_state, reward and whether done or not
+        obs = next_state
+
+        if done is True:
+            print("\n Episode #{} ended in {} steps.".format(episode, step+1))
+            break
+        
