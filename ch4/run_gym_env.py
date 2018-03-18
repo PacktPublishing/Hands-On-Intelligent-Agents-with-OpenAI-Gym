2cee54bab9befdd9045e928dd2ad919847630985
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-03-17 21:07:01 -0400

    ch4. Renamed run_env to run_gym_env

--- ch4/run_gym_env.py
+++ ch4/run_gym_env.py
@@ -3,7 +3,7 @@
 import gym
 import sys
 
-def run_env(argv):
+def run_gym_env(argv):
     env = gym.make(argv[1]) # Name of the environment supplied as 1st argument
     env.reset()
     for _ in range(int(argv[2])): # Number of steps to be run supplied as 2nd argument
@@ -12,4 +12,4 @@ def run_env(argv):
     env.close()
     
 if __name__ == "__main__":
-    run_env(sys.argv)
+    run_gym_env(sys.argv)
