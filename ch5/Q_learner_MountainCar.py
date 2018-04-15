14d2e7e586720a909bdb2e371607b7932a7d577f
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-04-15 14:14:58 -0400

    A simple and complete version of the Q learning agent. Solves the Mountain Car problem in this script

---
+++ ch5/Q_learner_MountainCar.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env/ python
+"""
+q_learner.py
+An easy-to-follow script to train, test and evaluate a Q-learning agent on the Mountain Car
+problem using the OpenAI Gym.
+This is part of the Book on "Getting started with OpenAI Gym"
+Author: Praveen Palanisamy
+"""
+import gym
+import numpy as np
+
+MAX_NUM_EPISODES = 50000
+STEPS_PER_EPISODE = 200 #  This is specific to MountainCar. May change with env
+EPSILON_MIN = 0.005
+max_num_steps = MAX_NUM_EPISODES * STEPS_PER_EPISODE
+EPSILON_DECAY = 500 * EPSILON_MIN / max_num_steps
+ALPHA = 0.05  # Learning rate
+GAMMA = 0.98  # Discount factor
+NUM_DISCRETE_BINS = 30  # Number of bins to Discretize each observation dim
+
+
+class Q_Learner(object):
+    def __init__(self, env):
+        self.obs_shape = env.observation_space.shape
+        self.obs_high = env.observation_space.high
+        self.obs_low = env.observation_space.low
+        self.obs_bins = NUM_DISCRETE_BINS  # Number of bins to Discretize each observation dim
+        self.bin_width = (self.obs_high - self.obs_low) / self.obs_bins
+        self.action_shape = env.action_space.n
+        # Create a multi-dimensional array (aka. Table) to represent the
+        # Q-values
+        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1,
+                           self.action_shape))  # (51 x 51 x 3)
+        self.alpha = ALPHA  # Learning rate
+        self.gamma = GAMMA  # Discount factor
+        self.epsilon = 1.0
+
+    def discretize(self, obs):
+        return tuple(((obs - self.obs_low) / self.bin_width).astype(int))
+
+    def get_action(self, obs):
+        discretized_obs = self.discretize(obs)
+        # Epsilon-Greedy action selection
+        if self.epsilon > EPSILON_MIN:
+            self.epsilon -= EPSILON_DECAY
+        if np.random.random() > self.epsilon:
+            return np.argmax(self.Q[discretized_obs])
+        else:  # Choose a random action
+            return np.random.choice([a for a in range(self.action_shape)])
+
+    def learn(self, obs, action, reward, next_obs):
+        discretized_obs = self.discretize(obs)
+        discretized_next_obs = self.discretize(next_obs)
+        td_target = reward + self.gamma * np.max(self.Q[discretized_next_obs])
+        td_error = td_target - self.Q[discretized_obs][action]
+        self.Q[discretized_obs][action] += self.alpha * td_error
+
+def train(agent, env):
+    best_reward = -float('inf')
+    for episode in range(MAX_NUM_EPISODES):
+        done = False
+        obs = env.reset()
+        total_reward = 0.0
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, done, info = env.step(action)
+            agent.learn(obs, action, reward, next_obs)
+            obs = next_obs
+            total_reward += reward
+        if total_reward > best_reward:
+            best_reward = total_reward
+        print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,
+                                     total_reward, best_reward, agent.epsilon))
+    # Return the trained policy
+    return np.argmax(agent.Q, axis=2)
+
+
+def test(agent, env, policy):
+    done = False
+    obs = env.reset()
+    total_reward = 0.0
+    while not done:
+        action = policy[agent.discretize(obs)]
+        next_obs, reward, done, info = env.step(action)
+        obs = next_obs
+        total_reward += reward
+    return total_reward
+
+
+if __name__ == "__main__":
+    env = gym.make('MountainCar-v0')
+    agent = Q_Learner(env)
+    learned_policy = train(agent, env)
+    # Use the Gym Monitor wrapper to evalaute the agent and record video
+    gym_monitor_path = "./gym_monitor_output"
+    env = gym.wrappers.Monitor(env, gym_monitor_path, force=True)
+    for _ in range(1000):
+        test(agent, env, learned_policy)
+    env.close()
+
