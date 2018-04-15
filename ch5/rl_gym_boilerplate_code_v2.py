0a6c150590d0e876adf7c8e178c74db60884234d
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-04-15 13:54:24 -0400

    Newer version of  rl gym boilerplate code

---
+++ ch5/rl_gym_boilerplate_code_v2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import gym
+env = gym.make("MountainCar-v0")
+MAX_NUM_EPISODES = 5000
+
+for episode in range(MAX_NUM_EPISODES):
+    done = False
+    obs = env.reset()
+    total_reward = 0.0 # To keep track of the total reward obtained in each episode
+    step = 0
+    while not done:
+        env.render()
+        action = env.action_space.sample()# Sample random action. This will be replaced by our agent's action when we start developing the agent algorithms
+        next_state, reward, done, info = env.step(action) # Send the action to the environment and receive the next_state, reward and whether done or not
+        total_reward += reward
+        step += 1
+        obs = next_state
+
+    print("\n Episode #{} ended in {} steps. total_reward={}".format(episode, step+1, total_reward))
+env.close()
