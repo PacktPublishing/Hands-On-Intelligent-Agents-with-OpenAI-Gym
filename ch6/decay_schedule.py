3a3e464b4b7284999ed05908fa9f20285439b5b1
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-04-29 09:35:53 -0400

    A decay schedule utility with LinearDecaySchedule class implementation

---
+++ ch6/decay_schedule.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+class LinearDecaySchedule(object):
+    def __init__(self, initial_value, final_value, max_steps):
+        assert initial_value > final_value, "initial_value should be < final_value"
+        self.initial_value = initial_value
+        self.decay_factor = (initial_value - final_value) / max_steps
+
+    def __call__(self, step_num):
+        current_value = self.initial_value - self.decay_factor * step_num
+        return current_value
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    epsilon_initial = 1.0
+    epsilon_final = 0.05
+    MAX_NUM_EPISODES = 1000
+    MAX_STEPS_PER_EPISODE = 500
+    linear_sched = LinearDecaySchedule(initial_value = epsilon_initial,
+                                    final_value = epsilon_final,
+                                    max_steps = MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)
+    epsilon = [linear_sched(step) for step in range(MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)]
+    plt.plot(epsilon)
+    plt.show()
