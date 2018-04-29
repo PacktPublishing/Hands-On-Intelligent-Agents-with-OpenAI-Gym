46d16203cd4f6702ac2e646eda16da9c332e35a8
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-04-29 09:38:38 -0400

    A Q learner with Neural network function approximation - Uses an Experience Replay memory (implemented using list for simplicity) - Uses a linear decay schedule for epsilon exploration - Learns from experience mini-batches samples from the experience replay memory - Additional improvement to follow

---
+++ ch6/deep_Q_learner.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import gym
+import random
+import torch
+from collections import namedtuple
+from torch.autograd import Variable
+import numpy as np
+from decay_schedule import LinearDecaySchedule
+
+env = gym.make("CartPole-v0")
+MAX_NUM_EPISODES = 10000
+MAX_STEPS_PER_EPISODE = 300
+REPLAY_BATCH_SIZE = 1000
+
+Experience = namedtuple("Experience", ['obs', 'action', 'reward', 'next_obs',
+                                       'done'])
+
+
+class NN1(torch.nn.Module):
+    def __init__(self, input_shape, output_shape, hidden_shape=40):
+        super(NN1, self).__init__()
+        self.linear1 = torch.nn.Linear(input_shape, hidden_shape)
+        self.linear2 = torch.nn.Linear(hidden_shape, hidden_shape)
+        self.out = torch.nn.Linear(hidden_shape, output_shape)
+
+    def forward(self, x):
+        x = Variable(torch.from_numpy(x)).float()
+        x = torch.nn.functional.relu(self.linear1(x))
+        x = self.out(x)
+        return x
+
+
+class Deep_Q_Learner(object):
+    def __init__(self, state_shape, action_shape, learning_rate=0.005,
+                 gamma=0.98):
+        self.state_shape = state_shape
+        self.action_shape = action_shape
+        self.gamma = gamma  # Agent's discount factor
+        self.learning_rate = learning_rate  # Agent's Q-learning rate
+        # self.Q is the Action-Value function. This agent represents Q using a
+        # Neural Network.
+        self.Q = NN1(state_shape, action_shape)
+        self.Q_optimizer = torch.optim.Adam(self.Q.parameters(), lr=1e-3)
+        # self.policy is the policy followed by the agent. This agents follows
+        # an epsilon-greedy policy w.r.t it's Q estimate.
+        self.policy = self.epsilon_greedy_Q
+        self.epsilon_max = 1.0
+        self.epsilon_min = 0.05
+        self.epsilon_decay = LinearDecaySchedule(initial_value=self.epsilon_max,
+                                    final_value=self.epsilon_min,
+                                    max_steps=MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)
+        self.step_num = 0
+                
+        self.memory = list()
+
+    def get_action(self, observation):
+        return self.policy(observation)
+
+    def epsilon_greedy_Q(self, observation):
+        # Decay Epsilion/exploratin as per schedule
+        if random.random() < self.epsilon_decay(self.step_num):
+            action = random.choice([i for i in range(self.action_shape)])
+        else:
+            action = np.argmax(self.Q(observation).data.numpy())
+
+
+        return action
+
+    def learn(self, s, a, r, s_next, done):
+        # TD(0) Q-learning
+        if done:  # End of episode
+            td_target = reward + 0.0  # Set the value of terminal state to zero
+        else:
+            td_target = r + self.gamma * torch.max(self.Q(s_next))
+        td_error = td_target - self.Q(s)[a]
+        # Update Q estimate
+        #self.Q(s)[a] = self.Q(s)[a] + self.learning_rate * td_error
+        self.Q_optimizer.zero_grad()
+        td_error.backward()
+        self.Q_optimizer.step()
+
+    def learn_from_batch_experience(self, experience):
+        batch_xp = Experience(*zip(*experience))
+        obs_batch = np.array(batch_xp.obs)
+        action_batch = np.array(batch_xp.action)
+        reward_batch = np.array(batch_xp.reward)
+        next_obs_batch = np.array(batch_xp.next_obs)
+        done_batch = np.array(batch_xp.done)
+
+        done_true_idx = np.where(done_batch)
+        done_false_idx = np.where(~done_batch)
+        td_target = np.zeros_like(reward_batch)
+
+        td_target[done_true_idx] = reward_batch[done_true_idx]
+        td_target[done_false_idx] = reward_batch[done_false_idx] + \
+            np.tile(self.gamma, len(done_false_idx)) * \
+            self.Q(next_obs_batch[done_false_idx]).max(1)[0].data
+
+        td_target = Variable(torch.from_numpy(td_target))
+        action_idx = Variable(torch.from_numpy(action_batch))
+        td_error = td_target.float() - self.Q(obs_batch).gather(1, action_idx.view(-1, 1))
+
+        self.Q_optimizer.zero_grad()
+        td_error.mean().backward()
+        self.Q_optimizer.step()
+
+    def replay_experience(self, batch_size=REPLAY_BATCH_SIZE):
+        experience_batch = random.sample(self.memory, batch_size)
+        self.learn_from_batch_experience(experience_batch)
+
+
+if __name__ == "__main__":
+    observation_shape = env.observation_space.shape[0]
+    action_shape = env.action_space.n
+    agent = Deep_Q_Learner(observation_shape, action_shape)
+    first_episode = True
+    episode_rewards = list()
+    for episode in range(MAX_NUM_EPISODES):
+        obs = env.reset()
+        cum_reward = 0.0  # Cumulative reward
+        for step in range(MAX_STEPS_PER_EPISODE):
+            # env.render()
+            action = agent.get_action(obs)
+            next_obs, reward, done, info = env.step(action)
+            #agent.learn(obs, action, reward, next_obs, done)
+            agent.memory.append(Experience(obs, action, reward, next_obs, done))
+
+            obs = next_obs
+            cum_reward += reward
+
+            if done is True:
+                if first_episode:  # Initialize max_reward at the end of first episode
+                    max_reward = cum_reward
+                    first_episode = False
+                episode_rewards.append(cum_reward)
+                if cum_reward > max_reward:
+                    max_reward = cum_reward
+                print("\nEpisode#{} ended in {} steps. reward ={} ; mean_reward={} best_reward={}".
+                      format(episode, step+1, cum_reward, np.mean(episode_rewards), max_reward))
+                if len(agent.memory) > 2 * REPLAY_BATCH_SIZE:
+                    agent.replay_experience()
+                break
+    env.close()
