e4fd49998f0102a2f0103d237b4941f91307257d
Praveen Palanisamy <praveenofpersia@gmail.com>
2018-04-30 16:16:40 -0400

    Clipped the decay to the desired minimum

--- ch6/decay_schedule.py
+++ ch6/decay_schedule.py
@@ -4,18 +4,21 @@ class LinearDecaySchedule(object):
     def __init__(self, initial_value, final_value, max_steps):
         assert initial_value > final_value, "initial_value should be < final_value"
         self.initial_value = initial_value
+        self.final_value = final_value
         self.decay_factor = (initial_value - final_value) / max_steps
 
     def __call__(self, step_num):
         current_value = self.initial_value - self.decay_factor * step_num
+        if current_value < self.final_value:
+            current_value = self.final_value
         return current_value
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     epsilon_initial = 1.0
     epsilon_final = 0.05
-    MAX_NUM_EPISODES = 1000
-    MAX_STEPS_PER_EPISODE = 500
+    MAX_NUM_EPISODES = 10000
+    MAX_STEPS_PER_EPISODE = 300
     linear_sched = LinearDecaySchedule(initial_value = epsilon_initial,
                                     final_value = epsilon_final,
                                     max_steps = MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)
