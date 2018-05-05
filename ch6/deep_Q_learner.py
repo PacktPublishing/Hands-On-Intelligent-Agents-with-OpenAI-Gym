1180442f7d690482ab4b796f10fe69b28d209563
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-05-05 13:32:43 -0400

    The Learning agent now uses an external json config file through the Parameter Manager to handle the configuration and hyper parameters

--- ch6/deep_Q_learner.py
+++ ch6/deep_Q_learner.py
@@ -5,27 +5,36 @@ import torch
 import random
 import numpy as np
 
+from utils.params_manager import ParamsManager
 from decay_schedule import LinearDecaySchedule
 from utils.experience_memory import Experience, ExperienceMemory
 from function_approximator.perceptron import SLP
 from function_approximator.cnn import CNN
 from tensorboardX import SummaryWriter
 from datetime import datetime
+from argparse import ArgumentParser
 
-ENV_NAME = "CartPole-v0"
-MAX_NUM_EPISODES = 70000
-MAX_STEPS_PER_EPISODE = 300  # Currently only used for calculating epsilon decay schedule
-REPLAY_BATCH_SIZE = 2000
-SEED = 555
-USE_CUDA = True
-USE_TARGET_NETWORK = True
-# Num steps after which target net is updated. A schedule can be used instead to vary the update freq
-TARGET_NETWORK_UPDATE_FREQ = 2000
-summary_file_name = "logs/DQL" + datetime.now().strftime("%y-%m-%d-%H-%M")
+args = ArgumentParser("Deep-Q-Learner arguments")
+args.add_argument("--params-file",
+                  help="Path to the parameters json file. Default is parameters.json",
+                  default="parameters.json",
+                  type=str,
+                  metavar="F")
+args.add_argument("--env-name",
+                  help="ID of the Atari environment available in OpenAI Gym",
+                  default="CartPole-v0",
+                  type=str,
+                  metavar="E")
+args = args.parse_args()
+
+params_manager= ParamsManager(args.params_file)
+ENV_NAME = args.env_name
+SEED = params_manager.get_agent_params()['seed']
+summary_file_name = params_manager.get_agent_params()['summary_filename_prefix'] + datetime.now().strftime("%y-%m-%d-%H-%M")
 writer = SummaryWriter(summary_file_name)
 global_step_num = 0
-
-device = torch.device("cuda" if torch.cuda.is_available() and USE_CUDA else "cpu")  # new in PyTorch 0.4
+# new in PyTorch 0.4
+device = torch.device("cuda" if torch.cuda.is_available() and params_manager.get_agent_params()['use_cuda'] else "cpu")
 torch.manual_seed(SEED)
 np.random.seed(SEED)
 if torch.cuda.is_available() and USE_CUDA:
@@ -33,8 +42,7 @@ if torch.cuda.is_available() and USE_CUDA:
 
 
 class Deep_Q_Learner(object):
-    def __init__(self, state_shape, action_shape, learning_rate=0.005,
-                 gamma=0.98):
+    def __init__(self, state_shape, action_shape, params):
         """
         self.Q is the Action-Value function. This agent represents Q using a Neural Network
         If the input is a single dimensional vector, uses a Single-Layer-Perceptron else if the input is 3 dimensional
@@ -47,8 +55,9 @@ class Deep_Q_Learner(object):
         """
         self.state_shape = state_shape
         self.action_shape = action_shape
-        self.gamma = gamma  # Agent's discount factor
-        self.learning_rate = learning_rate  # Agent's Q-learning rate
+        self.params = params
+        self.gamma = self.params['gamma']  # Agent's discount factor
+        self.learning_rate = self.params['lr']  # Agent's Q-learning rate
 
         if len(self.state_shape) == 1:  # Single dimensional observation/state space
             self.DQN = SLP
@@ -57,7 +66,7 @@ class Deep_Q_Learner(object):
 
         self.Q = self.DQN(state_shape, action_shape, device).to(device)
         self.Q_optimizer = torch.optim.Adam(self.Q.parameters(), lr=1e-3)
-        if USE_TARGET_NETWORK:
+        if self.params['use_target_network']:
             self.Q_target = self.DQN(state_shape, action_shape, device).to(device)
         # self.policy is the policy followed by the agent. This agents follows
         # an epsilon-greedy policy w.r.t it's Q estimate.
@@ -66,7 +75,8 @@ class Deep_Q_Learner(object):
         self.epsilon_min = 0.05
         self.epsilon_decay = LinearDecaySchedule(initial_value=self.epsilon_max,
                                     final_value=self.epsilon_min,
-                                    max_steps= 0.1 * MAX_NUM_EPISODES * MAX_STEPS_PER_EPISODE)
+                                    max_steps= 0.1 * self.params['max_num_episodes'] * \
+                                               self.params['max_steps_per_episode'])
         self.step_num = 0
                 
         self.memory = ExperienceMemory(capacity=int(1e6))  # Initialize an Experience memory with 1M capacity
@@ -112,8 +122,10 @@ class Deep_Q_Learner(object):
         next_obs_batch = np.array(batch_xp.next_obs)
         done_batch = np.array(batch_xp.done)
 
-        if USE_TARGET_NETWORK:
-            if self.step_num % TARGET_NETWORK_UPDATE_FREQ == 0:
+        if self.params['use_target_network']:
+            if self.step_num % self.params['target_network_update_freq'] == 0:
+                # The *update_freq is the Num steps after which target net is updated.
+                # A schedule can be used instead to vary the update freq.
                 self.Q_target.load_state_dict(self.Q.state_dict())
             td_target = reward_batch + ~done_batch * \
                 np.tile(self.gamma, len(next_obs_batch)) * \
@@ -133,7 +145,8 @@ class Deep_Q_Learner(object):
         writer.add_scalar("DQL/td_error", td_error.mean(), self.step_num)
         self.Q_optimizer.step()
 
-    def replay_experience(self, batch_size=REPLAY_BATCH_SIZE):
+    def replay_experience(self, batch_size = None):
+        batch_size = batch_size if batch_size is not None else self.params['replay_batch_size']
         experience_batch = self.memory.sample(batch_size)
         self.learn_from_batch_experience(experience_batch)
 
@@ -141,15 +154,16 @@ if __name__ == "__main__":
     env = gym.make(ENV_NAME)
     observation_shape = env.observation_space.shape
     action_shape = env.action_space.n
-    agent = Deep_Q_Learner(observation_shape, action_shape)
+    agent_params = params_manager.get_agent_params()
+    agent = Deep_Q_Learner(observation_shape, action_shape, agent_params)
     first_episode = True
     episode_rewards = list()
-    for episode in range(MAX_NUM_EPISODES):
+    for episode in range(agent_params['max_num_episodes']):
         obs = env.reset()
         cum_reward = 0.0  # Cumulative reward
         done = False
         step = 0
-        #for step in range(MAX_STEPS_PER_EPISODE):
+        #for step in range(agent_params['max_steps_per_episode']):
         while not done:
             #env.render()
             action = agent.get_action(obs)
@@ -175,7 +189,7 @@ if __name__ == "__main__":
                 writer.add_scalar("main/mean_ep_reward", np.mean(episode_rewards), episode)
                 writer.add_scalar("main/max_ep_rew", max_reward, episode)
 
-                if agent.memory.get_size() >= 2 * REPLAY_BATCH_SIZE:
+                if agent.memory.get_size() >= 2 * agent_params['replay_batch_size']:
                     agent.replay_experience()
                 break
     env.close()
