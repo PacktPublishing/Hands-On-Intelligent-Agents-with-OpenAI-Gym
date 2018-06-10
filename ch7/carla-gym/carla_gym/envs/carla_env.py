140eaf3836a6060969e610ecd8eb805c72d9cad9
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-06-09 23:44:21 -0400

    Ch7: carla gym CarlaEnv init method almost ready

--- ch7/carla-gym/carla_gym/envs/carla_env.py
+++ ch7/carla-gym/carla_gym/envs/carla_env.py
@@ -69,3 +69,78 @@ ENV_CONFIG = {
     "seed": 1
 }
 
+live_carla_processes = set()  # To keep track of all the Carla processes we launch to make the cleanup easier
+def cleanup():
+    print("Killing live carla processes", live_carla_processes)
+    for pgid in live_carla_processes:
+        os.killpg(pgid, signal.SIGKILL)
+atexit.register(cleanup)
+
+
+class CarlaEnv(gym.Env):
+    def __init__(self, config=ENV_CONFIG):
+        """
+        Carla Gym Environment class implementation. Creates an OpenAI Gym compatible driving environment based on
+        Carla driving simulator.
+        :param config: A dictionary with environment configuration keys and values
+        """
+        self.config = config
+        self.city = self.config["server_map"].split("/")[-1]
+        if self.config["enable_planner"]:
+            self.planner = Planner(self.city)
+
+        if config["use_depth_camera"]:
+            image_space = Box(
+                -1.0, 1.0, shape=(
+                    config["y_res"], config["x_res"],
+                    1 * config["framestack"]), dtype=np.float32)
+        else:
+            image_space = Box(
+                0.0, 255.0, shape=(
+                    config["y_res"], config["x_res"],
+                    3 * config["framestack"]), dtype=np.float32)
+        self.observation_space = Tuple(
+            [image_space,
+             Discrete(len(COMMANDS_ENUM)),  # next_command
+             Box(-128.0, 128.0, shape=(2,), dtype=np.float32)])  # forward_speed, dist to goal
+
+        self._spec = lambda: None
+        self._spec.id = "Carla-v0"
+        self._seed = ENV_CONFIG["seed"]
+
+        self.server_port = None
+        self.server_process = None
+        self.client = None
+        self.num_steps = 0
+        self.total_reward = 0
+        self.prev_measurement = None
+        self.prev_image = None
+        self.episode_id = None
+        self.measurements_file = None
+        self.weather = None
+        self.scenario = None
+        self.start_pos = None
+        self.end_pos = None
+        self.start_coord = None
+        self.end_coord = None
+        self.last_obs = None
+
+    def init_server(self):
+        print("Initializing new Carla server...")
+        # Create a new server process and start the client.
+        self.server_port = random.randint(10000, 60000)
+        self.server_process = subprocess.Popen(
+            [SERVER_BINARY, self.config["server_map"],
+             "-windowed", "-ResX=400", "-ResY=300",
+             "-carla-server",
+             "-carla-world-port={}".format(self.server_port)],
+            preexec_fn=os.setsid, stdout=open(os.devnull, "w"))
+        live_carla_processes.add(os.getpgid(self.server_process.pid))
+
+        for i in range(RETRIES_ON_ERROR):
+            try:
+                self.client = CarlaClient("localhost", self.server_port)
+                return self.client.connect()
+            except Exception as e:
+                print("Error connecting: {}, attempt {}".format(e, i))
+                time.sleep(2)
