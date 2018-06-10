edb313910f6a5d1323b02a61a220638d2f9924d1
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-06-10 00:54:45 -0400

    Ch7 carla gym CarlaEnv. Added the Carla PythonClient API into the codebase to make the repo standalone & easy for readers to use and play around with a tested version

---
+++ ch7/carla-gym/carla_gym/envs/carla/agent/forward_agent.py
@@ -0,0 +1,15 @@
+
+from .agent import Agent
+from ..client import VehicleControl
+
+
+class ForwardAgent(Agent):
+    """
+    Simple derivation of Agent Class,
+    A trivial agent agent that goes straight
+    """
+    def run_step(self, measurements, sensor_data, directions, target):
+        control = VehicleControl()
+        control.throttle = 0.9
+
+        return control
