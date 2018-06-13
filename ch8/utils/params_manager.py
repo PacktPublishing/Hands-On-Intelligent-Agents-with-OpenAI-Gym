4998553a32ee200b116753e25709b4f19622a8a4
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-06-13 18:43:36 -0400

    Added parameter manager class for the deep actor critic agent. Re-using the classes that the readers will be already familiar by chapter 8.

---
+++ ch8/utils/params_manager.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import json
+
+class ParamsManager(object):
+    def __init__(self, params_file):
+        """
+        A class to manage the Parameters. Parameters include configuration parameters and Hyper-parameters
+        :param params_file: Path to the parameters json file
+        """
+        self.params = json.load(open(params_file, 'r'))
+    def get_params(self):
+        """
+        Returns all the parameters
+        :return: The whole parameter dictionary
+        """
+        return self.params
+    def get_env_params(self):
+        """
+        Returns the environment configuration parameters
+        :return: A dictionary of configuration parameters used for the environment
+        """
+        return self.params['env']
+    def get_agent_params(self):
+        """
+        Returns the hyper-parameters and configuration parameters used by the agent
+        :return: A dictionary of parameters used by the agent
+        """
+        return self.params['agent']
+    def update_agent_params(self, **kwargs):
+        """
+        Update the hyper-parameters (and configuration parameters) used by the agent
+        :param kwargs:  Comma-separated, hyper-parameter-key=value pairs. Eg.: lr=0.005, gamma=0.98
+        :return: None
+        """
+        for key, value in kwargs.items():
+            if key in self.params['agent'].keys():
+                self.params['agent'][key] = value
+    def export_env_params(self, file_name):
+        """
+        Export the environment parameters to the specified file. Useful for logging experiment specific parameters
+        :param file_name: Name of the file to write the environment parameters to
+        :return:
+        """
+        with open(file_name, 'w') as f:
+            json.dump(self.params['env'], f)
+    def export_agent_params(self, file_name):
+        """
+        Export the agent parameters to the specified file. Useful for logging experiment specific parameters.
+        :param file_name: Name of the file to write the agent parameters to
+        :return:
+        """
+        with open(file_name, 'w') as f:
+            json.dump(self.params['agent'], f)
+
+
+if __name__ == "__main__":
+    print("Testing ParamsManager...")
+    param_file = "parameters.json"
+    params_manager = ParamsManager(param_file)
+    agent_params = params_manager.get_agent_params()
+    print("Agent params:")
+    for k, v in agent_params.items():
+        print(k, ":", v)
+    env_params = params_manager.get_env_params()
+    print("Environment parameters:")
+    for k, v in env_params.items():
+        print(k, ":", v)
+    params_manager.update_agent_params(lr=0.01, gamma=0.95)
+    updated_agent_params = params_manager.get_agent_params()
+    print("Updated Agent params:")
+    for k, v in updated_agent_params.items():
+        print(k, ":", v)
+
+    print("ParamsManager test completed.")
