c2b2334afdbdfdac526ce44c2ad5bf78ed0abf3a
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-06-25 20:11:11 -0400

    ch8: Added a Discrete Actor implementation

--- ch8/function_approximator/deep.py
+++ ch8/function_approximator/deep.py
@@ -44,6 +44,47 @@ class Actor(torch.nn.Module):
         return actor_mu, actor_sigma
 
 
+class DiscreteActor(torch.nn.Module):
+    def __init__(self, input_shape, actor_shape, device=torch.device("cpu")):
+        """
+        Deep convolutional Neural Network to represent Actor in an Actor-Critic algorithm
+        The Policy is parametrized using a categorical/discrete distribution with logits
+        The Actor's policy parameters (logits) are output by the deep CNN implemented
+        in this class.
+        :param input_shape: Shape of each of the observations
+        :param actor_shape: Shape of the actor's output. Typically the shape of the actions
+        :param device: The torch.device (cpu or cuda) where the inputs and the parameters are to be stored and operated
+        """
+        super(DiscreteActor, self).__init__()
+        self.device = device
+        self.layer1 = torch.nn.Sequential(torch.nn.Conv2d(input_shape[2], 128, 3, stride=1, padding=0),
+                                          torch.nn.ReLU())
+        self.layer2 = torch.nn.Sequential(torch.nn.Conv2d(128, 64, 3, stride=1, padding=0),
+                                          torch.nn.ReLU())
+        self.layer3 = torch.nn.Sequential(torch.nn.Conv2d(64, 32, 3, stride=1, padding=0),
+                                          torch.nn.ReLU())
+        self.layer4 = torch.nn.Sequential(torch.nn.Linear(32 * 78 * 78, 2048),
+                                          torch.nn.ReLU())
+        self.logits = torch.nn.Linear(2048, actor_shape)
+
+    def forward(self, x):
+        """
+        Forward pass through the Actor network. Takes batch_size x observations as input and produces mu and sigma
+        as the outputs
+        :param x: The observations
+        :return: Mean (mu) and Sigma (sigma) for a Gaussian policy
+        """
+        x.requires_grad_()
+        x = x.to(self.device)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = x.view(x.shape[0], -1)
+        x = self.layer4(x)
+        logits = self.logits(x)
+        return logits
+
+
 class Critic(torch.nn.Module):
     def __init__(self, input_shape, critic_shape=1, device=torch.device("cpu")):
         """
